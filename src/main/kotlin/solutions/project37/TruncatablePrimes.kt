4c096bad5e642213990956ab83e6999a3d2a6858
HexedAgain <gregory.astley@gmail.com>
2024-03-09 19:51:39 +0000

    tidy up and remove slower implementations

--- src/main/kotlin/solutions/project37/TruncatablePrimes.kt
+++ src/main/kotlin/solutions/project37/TruncatablePrimes.kt
@@ -3,7 +3,6 @@ package solutions.project37
 import extensions.*
 import solutions.NoArgSolution
 import utils.primes.PrimeChecker
-import utils.primes.PrimeSieve
 import kotlin.math.log10
 import kotlin.math.pow
 
@@ -17,63 +16,35 @@ import kotlin.math.pow
 class TruncatablePrimes(
     private val primesUpto: Int = 1000_000,
 ): NoArgSolution<Int> {
-    private lateinit var primes: Set<Int>
-    private val primeChecker = PrimeChecker()
     override val problemNumber = 37
     override val problemName = "Truncatable Primes"
 
-    var candidatePrimes: MutableList<Int> = mutableListOf()
+    private val primeChecker = PrimeChecker()
+    private var candidatePrimes: MutableList<Int> = mutableListOf()
 
     override fun solve(): Int {
-        return getBothWayTruncatablePrimes3().sum()
+        return getBothWayTruncatablePrimes().sum()
     }
 
-    fun getBothWayTruncatablePrimes3(): List<Int> {
+    fun getBothWayTruncatablePrimes(): List<Int> {
         if (primesUpto < 23) return listOf()
-        candidatePrimes = (23..99).filter { primeChecker.checkPrime(it) && isLeftTruncatable2(it) }.toMutableList()
+        candidatePrimes = (23..99).filter { primeChecker.checkPrime(it) && isLeftTruncatable(it) }.toMutableList()
         (3..log10(primesUpto.toDouble()).toInt()).forEach {
             val minPrime = 10.0.pow(it - 2)
-            val oldList = candidatePrimes.filter { p -> p > minPrime }
-            oldList.forEach { p ->
+            val newCandidateList = candidatePrimes.filter { p -> p > minPrime }
+            newCandidateList.forEach { p ->
                 listOf(1,3,7,5,9).forEach { digit ->
                     val newP = p * 10 + digit
-                    if (primeChecker.checkPrime(newP) && isLeftTruncatable2(newP)) {
+                    if (primeChecker.checkPrime(newP) && isLeftTruncatable(newP)) {
                         candidatePrimes.add(newP)
                     }
                 }
             }
         }
-        return candidatePrimes.filter { isRightTruncatable2(it) && it <= primesUpto }
-    }
-
-    fun getBothWayTruncatablePrimes(): List<Int> {
-        if (primesUpto < 23) return listOf()
-        val primesList = PrimeSieve(maxPrime = primesUpto).sieve()
-        primes = primesList.toSet()
-        return primesList.filter(::truncatablePredicate)
-    }
-
-    private fun truncatablePredicate(prime: Int): Boolean {
-        val lastDigit = prime % 10
-        val firstDigit = prime.firstDigit()
-        return prime >= 23 &&
-               (firstDigit == 2 || firstDigit == 3 || firstDigit == 5 || firstDigit == 7) &&
-               (lastDigit == 2 || lastDigit == 3 || lastDigit == 5 || lastDigit == 7) &&
-               isLeftTruncatable(prime) &&
-               isRightTruncatable(prime)
-
+        return candidatePrimes.filter { isRightTruncatable(it) && it <= primesUpto }
     }
 
     private fun isLeftTruncatable(prime: Int): Boolean {
-        var truncatedPrime = prime
-        while (truncatedPrime > 0) {
-            if (!primes.contains(truncatedPrime)) return false
-            truncatedPrime /= 10
-        }
-        return true
-    }
-
-    private fun isLeftTruncatable2(prime: Int): Boolean {
         var truncatedPrime = prime
         while (truncatedPrime > 0) {
             if (!primeChecker.checkPrime(truncatedPrime)) return false
@@ -83,17 +54,6 @@ class TruncatablePrimes(
     }
 
     private fun isRightTruncatable(prime: Int): Boolean {
-        var truncatedPrime = prime
-        var modulo = prime.clampToPowTen()
-        while (modulo > 0) {
-            if (!primes.contains(truncatedPrime)) return false
-            truncatedPrime %= modulo
-            modulo /= 10
-        }
-        return true
-    }
-
-    private fun isRightTruncatable2(prime: Int): Boolean {
         var truncatedPrime = prime
         var modulo = prime.clampToPowTen()
         while (modulo > 0) {
--- src/test/kotlin/solutions/problem37/TruncatablePrimesTest.kt
+++ src/test/kotlin/solutions/problem37/TruncatablePrimesTest.kt
@@ -8,39 +8,39 @@ import solutions.project37.TruncatablePrimes
 class TruncatablePrimesTest {
     @Test
     fun `bothWayTruncatablePrimes, if max prime is zero or negative, it returns emptyList`() {
-        assertEquals(emptyList<Int>(), TruncatablePrimes(primesUpto = 0).getBothWayTruncatablePrimes3())
-        assertEquals(emptyList<Int>(), TruncatablePrimes(primesUpto = -123).getBothWayTruncatablePrimes3())
+        assertEquals(emptyList<Int>(), TruncatablePrimes(primesUpto = 0).getBothWayTruncatablePrimes())
+        assertEquals(emptyList<Int>(), TruncatablePrimes(primesUpto = -123).getBothWayTruncatablePrimes())
     }
     @Test
     fun `bothWayTruncatablePrimes, if max prime is less than 23, it returns emptyList`() {
         (1 until 23).forEach {
-            assertEquals(emptyList<Int>(), TruncatablePrimes(primesUpto = it).getBothWayTruncatablePrimes3())
+            assertEquals(emptyList<Int>(), TruncatablePrimes(primesUpto = it).getBothWayTruncatablePrimes())
         }
     }
 
     @Test
     fun `bothWayTruncatablePrimes, if max prime is equal to 23, it returns list (23)`() {
-        val result = TruncatablePrimes(primesUpto = 23).getBothWayTruncatablePrimes3()
+        val result = TruncatablePrimes(primesUpto = 23).getBothWayTruncatablePrimes()
 
         assertEquals(listOf(23), result)
     }
 
     @Test
     fun `bothWayTruncatablePrimes, if max prime is 37, it returns list (23, 37)`() {
-        val result = TruncatablePrimes(primesUpto = 37).getBothWayTruncatablePrimes3()
+        val result = TruncatablePrimes(primesUpto = 37).getBothWayTruncatablePrimes()
 
         assertEquals(listOf(23, 37), result)
     }
 
     @Test
     fun `bothWayTruncatablePrimes, if max prime is 100, it returns list (23, 37, 53, 73)`() {
-        val result = TruncatablePrimes(primesUpto = 100).getBothWayTruncatablePrimes3()
+        val result = TruncatablePrimes(primesUpto = 100).getBothWayTruncatablePrimes()
 
         assertEquals(listOf(23, 37, 53, 73), result)
     }
     @Test
     fun `bothWayTruncatablePrimes, if max prime is 1000_000, it returns list (23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 739397)`() {
-        val result = TruncatablePrimes(primesUpto = 1000_000).getBothWayTruncatablePrimes3()
+        val result = TruncatablePrimes(primesUpto = 1000_000).getBothWayTruncatablePrimes()
 
         assertEquals(listOf(23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 739397), result)
     }
